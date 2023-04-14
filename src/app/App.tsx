07e0789f1214ab3022ed208d46491eedbc97a69c
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2023-04-13 17:16:18 -0700

    Updated Department (#8)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,26 +9,19 @@ import { RouterProvider } from "react-router";
 import "./styles/_login.scss";
 import awsmobile from "../aws-exports";
 import { Notifications } from "@mantine/notifications";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
   Amplify.configure(awsmobile);
   const { route } = useAuthenticator((context) => [context.route]);
-  const queryClient = new QueryClient();
   return (
     <MantineProvider>
       <ModalsProvider>
-        <QueryClientProvider client={queryClient}>
+        
           {route === "authenticated" ? (
-            <>
-              <Notifications />
-              <RouterProvider
-                router={router}
-                fallbackElement={
-                  <NavigationProgress autoReset progressLabel="Loading Page" />
-                }
-              />
-            </>
+            
+              <><Notifications /><RouterProvider
+            router={router}
+            fallbackElement={<NavigationProgress autoReset progressLabel="Loading Page" />} /></>
           ) : (
             <div className="login-template">
               <Authenticator
@@ -37,7 +30,6 @@ function App() {
               />
             </div>
           )}
-        </QueryClientProvider>
       </ModalsProvider>
     </MantineProvider>
   );
--- src/app/types/result.type.ts
+++ src/app/types/result.type.ts
@@ -1,7 +1,16 @@
 import { GraphQLError } from "graphql";
+import { Departments, Users } from "../../API";
 export interface GraphQLResult {
   items: Array< {} | null> | null | undefined;
   nextToken: string | null | undefined;
   errors: GraphQLError[] | undefined;
   extenstions: { [key: string]: any } | undefined;
+}
+
+export interface UserGraphQLResult extends Omit<GraphQLResult, 'items'> {
+  items: Users[] | null | undefined
+}
+
+export interface DepartmentGraphQLResult extends Omit<GraphQLResult, 'items'> {
+  items: Departments[] | null | undefined
 }
